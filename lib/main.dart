792a32299081c7f6b988b03a097714854bac5545
Minahil Fatima <fminahil16343@gmail.com>
2024-12-27 20:49:10 -0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:todo/views/starting-screens/splash-screen/splash-screen.dart';
 
 void main()  async {
@@ -14,7 +15,7 @@ void main()  async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: SplashScreen()
 
     );
