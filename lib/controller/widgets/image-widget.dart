8632fe2cc110217f055bbe3f9461d8dc61b91df9
Minahil Fatima <fminahil16343@gmail.com>
2024-12-28 19:59:06 -0800

    updated

--- lib/controller/widgets/image-widget.dart
+++ lib/controller/widgets/image-widget.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
   String image;
-  ImageWidget({super.key , required this.image});
+  ImageWidget({super.key, required this.image});
 
   @override
   Widget build(BuildContext context) {
-    return Image(image: AssetImage(image),
+    return Image(
+      image: AssetImage(image),
       height: 230,
     );
-
-
   }
 }
--- lib/controller/widgets/normal-text-widget.dart
+++ lib/controller/widgets/normal-text-widget.dart
@@ -5,17 +5,17 @@ import 'package:todo/controller/constants/app-colors/app-colors.dart';
 class NormalTextWidget extends StatelessWidget {
   String text;
   Color textColor;
-   NormalTextWidget({super.key , required this.text , required this.textColor});
+  NormalTextWidget({super.key, required this.text, required this.textColor});
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      text,style: GoogleFonts.poppins(
-      fontSize: 15,
-      fontWeight: FontWeight.w400,
-     color: textColor,
-
-    ),
+      text,
+      style: GoogleFonts.poppins(
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+        color: textColor,
+      ),
     );
   }
 }
