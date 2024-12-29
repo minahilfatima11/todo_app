4862f13de356f7aac77cb138372b9cff9d8f9596
Minahil Fatima <fminahil16343@gmail.com>
2024-12-28 16:25:23 -0800

    updated

--- lib/views/authentication/sign-in-screen.dart
+++ lib/views/authentication/sign-in-screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:todo/controller/constants/app-colors/app-colors.dart';
@@ -21,58 +23,65 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppColors.backgroundColor,
-        body : SingleChildScrollView(
-          child: Stack(
-            children: [
-              CircularContainerTop(),
-              CircularContainerLeft(),
-              BackArrowWidget(),
-              Column(
-                children: [
-                  SizedBox(height: 160,),
-                  BlackTextHeading(text: 'Welcome Back!'),
-                  SizedBox(height: 20,),
-                  ImageWidget(image: AppImages.loginImg),
-                  SizedBox(height: 24,),
-          
-                  TextFieldWidget(hintText : 'Enter Your Email Address' , controller: emailController),
-                  TextFieldWidget(hintText : 'Enter Your Password' , controller: passwordController),
-
-          
-                  SizedBox(height: 40,),
-                  isLoading ? SpinKitWaveSpinner(
-                    duration: const Duration(seconds: 3),
-
-                    color: AppColors.primarycolor,
-
-                  ) :
-
-                  ButtonWidget(text: 'Sign In', ontap:()
-                   async
-                  {
+      backgroundColor: AppColors.backgroundColor,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            CircularContainerTop(),
+            CircularContainerLeft(),
+            BackArrowWidget(),
+            Column(
+              children: [
+                SizedBox(height: 160),
+                BlackTextHeading(text: 'Welcome Back!'),
+                SizedBox(height: 20),
+                ImageWidget(image: AppImages.loginImg),
+                SizedBox(height: 24),
+                TextFieldWidget(
+                    hintText: 'Enter Your Email Address',
+                    controller: emailController),
+                TextFieldWidget(
+                    hintText: 'Enter Your Password',
+                    controller: passwordController),
+                SizedBox(height: 40),
+                isLoading
+                    ? SpinKitWaveSpinner(
+                  duration: const Duration(seconds: 3),
+                  color: AppColors.primarycolor,
+                )
+                    : ButtonWidget(
+                  text: 'Sign In',
+                  ontap: () async {
                     isLoading = true;
-
-                    setState((){
-                      //isLoading = false;
+                    setState(() {});
+                    await FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
+                        email: emailController.text.trim(),
+                        password: passwordController.text)
+                        .then((value) {
+                      isLoading = false;
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) => HomeScreen()));
+                      });
                     });
-
-                    Navigator.push(context , MaterialPageRoute(builder: (context) => HomeScreen()));
-                  }),
-                  SizedBox(height: 14,),
-          
-                ],
-              ),
-            ],
-          ),
-        )
+                  },
+                ),
+                SizedBox(height: 14),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
