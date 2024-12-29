4dd85db03893bf5a455807968f99c0744296d48b
Minahil Fatima <fminahil16343@gmail.com>
2024-12-28 19:54:56 -0800

    update

--- lib/controller/constants/app-colors/app-colors.dart
+++ lib/controller/constants/app-colors/app-colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primarycolor= Color(0xff7972a3);
+  static Color primarycolor = Color(0xff7972a3);
   static Color backgroundColor = Color(0xfff6f4f4);
   static Color secondaryColor = Color(0xffFFFFFF);
   static Color blackColor = Colors.black;
