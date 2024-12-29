723a51f3d53caf986591fa13dc375f824410fd9b
Minahil Fatima <fminahil16343@gmail.com>
2024-12-28 19:59:38 -0800

    updated

--- lib/controller/widgets/text-field-widget.dart
+++ lib/controller/widgets/text-field-widget.dart
@@ -3,35 +3,33 @@ import 'package:flutter/material.dart';
 import '../constants/app-colors/app-colors.dart';
 
 class TextFieldWidget extends StatelessWidget {
-  String hintText ;
+  String hintText;
   TextEditingController controller;
-  TextFieldWidget({super.key , required this.hintText , required this.controller});
+  TextFieldWidget(
+      {super.key, required this.hintText, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0 , vertical: 10),
-        child: Container(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
+      child: Container(
           decoration: BoxDecoration(
             color: AppColors.whiteColor,
             borderRadius: BorderRadius.circular(11),
           ),
           child: Padding(
-            padding: const EdgeInsets.only(left:12.0),
+            padding: const EdgeInsets.only(left: 12.0),
             child: TextFormField(
               style: TextStyle(
                 fontSize: 14,
               ),
               controller: controller,
               decoration: InputDecoration(
-                  hintText: hintText,
-
+                hintText: hintText,
                 border: InputBorder.none,
               ),
             ),
-          )
-            ),
-      );
+          )),
+    );
   }
 }
